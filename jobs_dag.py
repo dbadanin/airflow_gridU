aef43277bd1776931c09194b8f0f388049c84d99
dbadanin <dbadanin@griddynamics.com>
2021-05-14 16:20:29 +0400

    init commit

---
+++ jobs_dag.py
@@ -0,0 +1,8 @@
+
+from datetime import datetime
+import airflow as af
+config = {
+   'dag_id_1': {'schedule_interval': "", "start_date": datetime(2018, 11, 11)},  
+   'dag_id_2': {'schedule_interval': "", "start_date": datetime(2018, 11, 11)},  
+   'dag_id_3':{'schedule_interval': "", "start_date": datetime(2018, 11, 11)}}
+
