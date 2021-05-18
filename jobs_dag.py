661f0499a15c337cdf84d50ca63ffc6ee86a635a
dbadanin <dbadanin@griddynamics.com>
2021-05-18 20:43:45 +0400

    branching and dynamic creation

--- jobs_dag.py
+++ jobs_dag.py
@@ -3,6 +3,7 @@ from datetime import datetime, timedelta
 from airflow import DAG
 from airflow.operators.python import PythonOperator
 from airflow.operators.dummy import DummyOperator
+from airflow.operators.python import BranchPythonOperator
 
 DBS = ["DB-1", "DB-2", "DB-3"]
 config = {
@@ -32,29 +33,57 @@ default_args = {
     # "sla_miss_callback": yet_another_function,
     # "trigger_rule": "all_success"
 }
+def create_dag(dag_id,
+               schedule_unterval_custom,
+               start_date_custom,
+               database_name,
+               default_args):
+    
+    def print_process_start(dag_id, database):
+        """Print information about processing steps"""
+        print(f"{dag_id} start processing tables in database: {database}")
 
+    def check_table_exists(cond):
+        if cond == True:
+            return "insert_row"
+        else:
+            return "create_table"
 
-
-for dag_id in config: 
-   with DAG(dag_id,
+    dag = DAG(dag_id,
         default_args=default_args,
         description=f"DAG in the loop {dag_id}",
-        schedule_interval=config[dag_id]["schedule_interval"],
-        start_date=config[dag_id]["start_date"],
+        schedule_interval=schedule_unterval_custom,
+        start_date=start_date_custom,
         tags=["example"],
-    ) as dag:
+    )
+    with dag:
+        
+        task_logs = PythonOperator(
+        task_id=f"print_logs_{dag_id}",
+        python_callable=print_process_start,
+        op_kwargs={"dag_id": dag_id, "database": database_name},
+        )
 
-         def print_logs(dag_id, database):
-            """Print information about processing steps"""
-            print(f"{dag_id} start processing tables in database: {database}")
-         task_logs = PythonOperator(
-         task_id=f"print_logs_{dag_id}",
-         python_callable=print_logs,
-         op_kwargs={"dag_id": dag_id, "database": config[dag_id]["database"]},
-         )
+        task_check_table = BranchPythonOperator(
+            task_id="check_table_exist",
+            python_callable=check_table_exists,
+            dag=dag)
 
-         task_ins = DummyOperator(task_id=f"insert_new_row_{dag_id}")
+        task_create_table = DummyOperator(task_id=f"create_table")
 
-         task_query = DummyOperator(task_id=f"query_the_table_{dag_id}")
+        task_ins = DummyOperator(task_id=f"insert_row")
 
-         task_logs >> task_ins >> task_query
+        task_query = DummyOperator(task_id=f"query_the_table")
+
+        task_logs >> task_check_table >> [task_create_table, task_ins] >> task_query
+
+    return dag
+
+for dag_id in config: 
+    globals()[dag_id] = create_dag(dag_id,
+                                  config[dag_id]["schedule_interval"],
+                                  config[dag_id]["start_date"],
+                                  config[dag_id]["database"],
+                                  default_args)
+         
+         
